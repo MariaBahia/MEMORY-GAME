c6fd988a1656b8edf501984e3549979a3045b3eb
MARIA BAHIA <hejmariabahia@gmail.com>
2017-09-26 09:34:15 +0200

    09:35
    Start.

---
+++ src/game.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Card from "./card"
+import "./card.css"
+
+const Game = () => (
+<div className="game">
+<h1> MEMORY GAME</h1>
+<Card src="/images/dog1.jpeg"/>
+<Card src="/images/dog2.jpeg"/>
+<Card src="/images/dog3.jpeg"/>
+<Card src="/images/dog4.jpeg"/>
+<Card src="/images/dog5.jpeg"/>
+<Card src="/images/dog6.jpeg"/>
+<Card src="/images/dog7.jpeg"/>
+<Card src="/images/dog8.jpeg"/>
+</div>
+)
+
+export default Game
