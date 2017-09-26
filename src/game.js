bd08a24d770421d7de80514c474c4ed309f6801a
MARIA BAHIA <hejmariabahia@gmail.com>
2017-09-26 15:34:59 +0200

    14:32
    Renderar bild

--- src/game.js
+++ src/game.js
@@ -3,19 +3,46 @@ import Card from "./card"
 import "./card.css"
 import Counter from "./counter"
 
-const Game = () => (
-<div className="game">
-<h1> MEMORY GAME</h1>
-<Counter/>
-<Card src="/images/dog1.jpeg"/>
-<Card src="/images/dog2.jpeg"/>
-<Card src="/images/dog3.jpeg"/>
-<Card src="/images/dog4.jpeg"/>
-<Card src="/images/dog5.jpeg"/>
-<Card src="/images/dog6.jpeg"/>
-<Card src="/images/dog7.jpeg"/>
-<Card src="/images/dog8.jpeg"/>
-</div>
-)
 
-export default Game
+const photos = [                                            //array of photos
+  "/images/dog1.jpeg",
+  "/images/dog2.jpeg",
+  "/images/dog3.jpeg",
+  "/images/dog4.jpeg",
+  "/images/dog5.jpeg",
+  "/images/dog6.jpeg",
+    "/images/dog7.jpeg",
+      "/images/dog8.jpeg",
+  ]
+
+class Game extends React.Component {      // class with react libary functionality
+
+  constructor(props)              //  game is initialized
+  {
+    super(props)                  //makes sure that react compontent get initialized
+    this.state = {
+      cards: this.setupGame()
+    }
+  }
+
+    setupGame = () => {
+      return photos.map(photoUrl => {
+      return {src: photoUrl, isFlipped: false, isMatched: false} // object for each photo
+
+      })
+    }
+
+    renderCard = (something) => {
+      return <Card src={something.src} />
+    }
+    render ()  {
+      return (
+        <div className="game">
+        <h1> MEMORY GAME</h1>
+      {this.state.cards.map(this.renderCard)}
+        </div>
+      )
+    }
+  }
+
+  export default Game
